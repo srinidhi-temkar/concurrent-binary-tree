1b9c8f1f8ef577886f6a36d739f07cb9cc2d7173
abhay-bharat <a.p.s.77abhay@gmail.com>
2020-06-30 19:59:02 +0530

    Menu driven code

--- bt.cpp
+++ bt.cpp
@@ -1,4 +1,5 @@
 #include<bits/stdc++.h>
+#include<pthread.h>
 using namespace std;
 
 vector<int> bt;
@@ -47,28 +48,73 @@ void bt_delete(int ele) {
     cout << ele << " not found in the binary tree!" << endl;
 }
 
+int bt_search(int ele) {
+    for(vector<int>::iterator i = bt.begin(); i != bt.end(); i++) {
+        if(*i == ele) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 int main() {
-    bt_insert(10);
-    bt_insert(9);
-    bt_insert(7);
-    bt_insert(5);
-    bt_insert(14);
-    bt_insert(21);
-    bt_level_order_traversal(); 
-    bt_delete(9);
-    bt_level_order_traversal();
-    bt_delete(100);
-    bt_delete(10);
-    bt_level_order_traversal();
-    bt_delete(9);
-    bt_level_order_traversal();
-    bt_delete(7);
-    bt_level_order_traversal();
-    bt_delete(5);
-    bt_level_order_traversal();
-    bt_delete(14);
-    bt_level_order_traversal();
-    bt_delete(21);
-    bt_level_order_traversal();
-    bt_delete(21);
+    // bt_insert(10);
+    // bt_insert(9);
+    // bt_insert(7);
+    // bt_insert(5);
+    // bt_insert(14);
+    // bt_insert(21);
+    // bt_level_order_traversal(); 
+    // bt_delete(9);
+    // bt_level_order_traversal();
+    // bt_delete(100);
+    // if(bt_search(10)) {
+    //     cout << 10 << " found" << endl;
+    // } else {
+    //     cout << 10 << " not found" << endl;
+    // }
+    // bt_delete(10);
+    // if(bt_search(10)) {
+    //     cout << 10 << " found" << endl;
+    // } else {
+    //     cout << 10 << " not found" << endl;
+    // }
+    // bt_level_order_traversal();
+    // bt_delete(9);
+    // bt_level_order_traversal();
+    // bt_delete(7);
+    // bt_level_order_traversal();
+    // bt_delete(5);
+    // bt_level_order_traversal();
+    // bt_delete(14);
+    // bt_level_order_traversal();
+    // bt_delete(21);
+    // bt_level_order_traversal();
+    // bt_delete(21);
+    pthread_t id;
+    int choice, ele, found;
+    while(1)
+    {
+        cin>>choice;
+        switch(choice)
+        {
+            case 1 : cin>>ele;
+                     bt_insert(ele);
+                     break;
+
+            case 2 : cin>>ele;
+                     bt_delete(ele);
+                     break;
+
+            case 3 : cin>>ele;
+                     cout<<bt_search(ele)<<"\n";
+                     break;
+
+            case 4 : bt_level_order_traversal();
+                     break;
+
+            default : exit(0);
+        }
+    }
+    return 0;
 }
